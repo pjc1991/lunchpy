8aed8cb0d0bbbe5d18f36213173f302362f47f1c
pjc1991 <whdekf@gmail.com>
2022-12-22 22:13:04 +0900

    refactor : config.py import change

--- handler.py
+++ handler.py
@@ -8,8 +8,7 @@ import lunchpy.stores.store_collector as store_collector
 def hello(event, context):
     stores = store_collector.get_matzips()
     recommend_msg = recommend.recommend(stores)
-    text = slack_util.post_slack_message(
-        recommend_msg['message'], config.channel_id, config.slack_token)
+    text = slack_util.post_slack_message(recommend_msg['message'])
     return text
 
 
--- lunchpy/slack/slack_util.py
+++ lunchpy/slack/slack_util.py
@@ -1,27 +1,22 @@
 from slack_sdk import WebClient
-
-# post slack message to certain channel
-# get channel id from config.py
-# get slack token from config.py
-
-
-def post_slack_message(text, channel_id, slack_token):
+import lunchpy.config as config
+def post_slack_message(text):
 
     # if(config.slack_token == "" or config.channel_id == ""):
     #     # if slack token or channel id is not set, throw error
     #     raise Exception("Slack token or channel id is not set")
 
     # get slack client
-    client = WebClient(token=slack_token)
+    client = WebClient(token=config.slack_token)
 
     # if not joined yet, join the channel
-    if not client.conversations_join(channel=channel_id):
+    if not client.conversations_join(channel=config.channel_id):
         raise Exception("Failed to join channel")
 
     # print message
     print(f"slackUtil.text: {text}")
 
     # post message to slack
-    client.chat_postMessage(channel=channel_id, text=text)
+    client.chat_postMessage(channel=config.channel_id, text=text)
 
     return text
